05f889b9bcc5e71e7bc860415b14c45676d2ecf0
Documenter.jl <documenter@juliadocs.github.io>
2023-04-07 02:38:52 +0000

    build based on 8e3e1d0

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = OkRegistrator","category":"page"},{"location":"#OkRegistrator","page":"Home","title":"OkRegistrator","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for OkRegistrator.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [OkRegistrator]","category":"page"}]
+}
